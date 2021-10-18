c7e149dc7d8affbb691f2aecb95e76536ae42636
AkhileshEdd <58221408+AkhileshEdd@users.noreply.github.com>
2021-10-18 23:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+// var a=prompt("Enter your name...!!!");
+console.log(a);
+
+var a,b;
+function add(){
+    alert("Enter two numbers");
+    prompt(a);
+    prompt(b);
+    var c = Number(a)+Number(b);
+    console.log(c);
+
+}
+add();
